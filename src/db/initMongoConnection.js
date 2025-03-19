3265f76d3e915042cf813fdcb857125be56ae58f
Anna <iaanya27@gmail.com>
2025-03-19 14:03:20 +0200

    added mongoose and db connection

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const DB_URI =
+  'mongodb+srv://Annatest:Annatest22@cluster-questionnaire.8refb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-Questionnaire';
+
+async function initMongoConnection() {
+  try {
+    await mongoose.connect(DB_URI);
+    console.log('Mongo connection successfully established!');
+  } catch (e) {
+    console.log('Error while setting up mongo connection', e);
+    throw e;
+  }
+}
+export { initMongoConnection };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import { initMongoConnection } from './db/initMongoConnection.js';
 
 const app = express();
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
-});
+const bootstrap = async () => {
+  try {
+    await initMongoConnection();
+    app.listen(8080, () => {
+      console.log('Server is running on port 8080');
+    });
+  } catch (error) {
+    console.log('Error while connecting to MongoDB', error);
+  }
+};
+
+bootstrap();
